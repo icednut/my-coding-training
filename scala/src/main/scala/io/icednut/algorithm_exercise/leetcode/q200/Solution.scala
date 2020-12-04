99daaaa87bbea3a0827cc389f1e0ffd67098c17d
will.109 <will.109@kakaopaycorp.com>
2020-12-04 18:54:25 +0900

    섬개수 문제 다른 방식으로 해결

--- scala/src/main/scala/io/icednut/algorithm_exercise/leetcode/q200/Solution.scala
+++ scala/src/main/scala/io/icednut/algorithm_exercise/leetcode/q200/Solution.scala
@@ -10,10 +10,11 @@ object Solution {
 
     for (i <- 0 until grid.length) {
       for (j <- 0 until grid(i).length) {
-        if (isNotVisited((i, j), visited) && grid(i)(j) == '1') {
+        if (!isVisited((i, j), visited) && grid(i)(j) == '1') {
           val stack = new mutable.Stack[(Int, Int)]()
 
-          visited = dfs(grid, (i, j), stack, visited)
+          push(stack, (i, j), visited)
+          visited = dfs(grid, stack, visited)
           numIslands += 1
         }
       }
@@ -23,38 +24,42 @@ object Solution {
   }
 
   private def dfs(grid: Array[Array[Char]],
-                  position: (Int, Int),
                   stack: mutable.Stack[(Int, Int)],
                   visited: Array[(Int, Int)]): Array[(Int, Int)] = {
-    if (position._2 > 0 && grid(position._1)(position._2 - 1) == '1') {
-      push(stack, (position._1, position._2 - 1), visited)
-    }
-    if (position._2 < grid(position._1).length - 1 && grid(position._1)(position._2 + 1) == '1') {
-      push(stack, (position._1, position._2 + 1), visited)
-    }
-    if (position._1 > 0 && grid(position._1 - 1)(position._2) == '1') {
-      push(stack, (position._1 - 1, position._2), visited)
-    }
-    if (position._1 < grid.length - 1 && grid(position._1 + 1)(position._2) == '1') {
-      push(stack, (position._1 + 1, position._2), visited)
-    }
-
-    val newVisited = visited :+ position
     val newStandard = Try(stack.pop()).toOption
 
-    if (newStandard.isDefined) {
-      dfs(grid, newStandard.get, stack, newVisited)
-    } else {
-      newVisited
+    newStandard match {
+      case Some(position) =>
+        var newVisited = visited :+ position
+
+        if (position._2 > 0 && grid(position._1)(position._2 - 1) == '1') {
+          push(stack, (position._1, position._2 - 1), newVisited)
+          newVisited = dfs(grid, stack, newVisited)
+        }
+        if (position._2 < grid(position._1).length - 1 && grid(position._1)(position._2 + 1) == '1') {
+          push(stack, (position._1, position._2 + 1), newVisited)
+          newVisited = dfs(grid, stack, newVisited)
+        }
+        if (position._1 > 0 && grid(position._1 - 1)(position._2) == '1') {
+          push(stack, (position._1 - 1, position._2), newVisited)
+          newVisited = dfs(grid, stack, newVisited)
+        }
+        if (position._1 < grid.length - 1 && grid(position._1 + 1)(position._2) == '1') {
+          push(stack, (position._1 + 1, position._2), newVisited)
+          newVisited = dfs(grid, stack, newVisited)
+        }
+        newVisited
+      case None =>
+        visited
     }
   }
 
   private def push(stack: mutable.Stack[(Int, Int)], position: (Int, Int), visited: Array[(Int, Int)]): Unit =
-    if (isNotVisited(position, visited)) {
+    if (!isVisited(position, visited)) {
       stack.push(position)
     }
 
-  private def isNotVisited(position: (Int, Int), visited: Array[(Int, Int)]) = {
-    visited.toSeq.find(elem => elem._1 == position._1 && elem._2 == position._2).isEmpty
+  private def isVisited(position: (Int, Int), visited: Array[(Int, Int)]) = {
+    visited.toSeq.exists(elem => elem._1 == position._1 && elem._2 == position._2)
   }
 }
